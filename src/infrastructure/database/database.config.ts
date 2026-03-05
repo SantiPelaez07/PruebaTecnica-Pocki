4d71d708ae828d25a63b3f21f5163d3268e13eb4
SantiPelaez07 <santipelaez0@gmail.com>
2026-03-05 18:10:14 -0500

    Project initialization, hexagonal architecture folder structure and database connection setup.

---
+++ src/infrastructure/database/database.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const databaseConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: '1234',
+  database: 'whatsapp_bot',
+  autoLoadEntities: true,
+  synchronize: true,
+};
